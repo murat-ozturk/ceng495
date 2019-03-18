24f93cc71ad55d3f33b72b476010fa444b7cf3f4
Murat Ozturk <e188177@gmail.com>
2019-03-18 19:39:43 +0300

    Create Ceng495Hw1Application.java

---
+++ src/main/java/com/ceng/cloud/Ceng495Hw1Application.java
@@ -0,0 +1,13 @@
+package com.ceng.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ceng495Hw1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ceng495Hw1Application.class, args);
+	}
+
+}
